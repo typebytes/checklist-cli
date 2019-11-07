74f831f12f641e49edd76bfd0c13708143f5ce88
Younes JAAIDI <yjaaidi@gmail.com>
2019-11-07 09:48:19 +0100

    🚧 add checklist cli - fix dev build scripts

--- tools/checklist-cli.ts
+++ tools/checklist-cli.ts
@@ -10,21 +10,27 @@ export async function main() {
   const contentRelativePath = process.argv[2];
 
   if (!contentRelativePath) {
-    console.log(`Usage: checklist-cli CONTENT_PATH [--skip-app-copy]`);
+    console.log(`Usage: checklist-cli CONTENT_PATH [--dev]`);
     process.exit(1);
     return;
   }
 
-  const skipAppCopy = process.argv.includes('--skip-app-copy');
+  const isDev = process.argv.includes('--dev');
 
   const contentPath = path.join(process.cwd(), contentRelativePath);
+
+  const srcPath = path.join(process.cwd(), 'src');
   const distBrowserPath = path.join(process.cwd(), 'dist', 'browser');
 
-  if (!skipAppCopy) {
+  /* Copy to src assets when in dev mode. */
+  const assetsPath = path.join(isDev ? srcPath : distBrowserPath, 'assets');
+
+  /* Don't copy app when in dev mode. */
+  if (!isDev) {
     await _copyApp({ distBrowserPath });
   }
-  await _copyConfig({ contentPath, distBrowserPath });
-  await _buildChecklist({ contentPath, distBrowserPath });
+  await _copyConfig({ contentPath, assetsPath });
+  await _buildChecklist({ contentPath, assetsPath });
 
   printSuccess('Content was successfully compiled', 'Done');
 }
@@ -36,19 +42,16 @@ export async function _copyApp({ distBrowserPath }) {
   printSuccess(`App was successfully generated in "${distBrowserPath}"`, 'Success');
 }
 
-export async function _copyConfig({ contentPath, distBrowserPath }) {
+export async function _copyConfig({ contentPath, assetsPath }) {
   const configFileName = 'config.json';
-  await promisify(copyFile)(
-    path.join(contentPath, configFileName),
-    path.join(distBrowserPath, 'assets', configFileName)
-  );
+  await promisify(copyFile)(path.join(contentPath, configFileName), path.join(assetsPath, configFileName));
   printSuccess(`config.json copied into app`, 'Success');
 }
 
-export async function _buildChecklist({ contentPath, distBrowserPath }) {
+export async function _buildChecklist({ contentPath, assetsPath }) {
   const checklist = await buildChecklist(contentPath);
 
-  dumpDataToDisk('content', checklist, path.join(distBrowserPath, 'assets'));
+  dumpDataToDisk('content', checklist, assetsPath);
 }
 
 main()
