08607ca1666e1bfb7068d69d909c805f5bc6cfcf
Younes JAAIDI <yjaaidi@gmail.com>
2019-11-07 08:07:29 +0100

    🚧 add checklist cli - update page title

--- src/app/config/config.effects.ts
+++ src/app/config/config.effects.ts
@@ -1,8 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Effect } from '@ngrx/effects';
-import { map } from 'rxjs/operators';
+import { select, Store } from '@ngrx/store';
+import { map, tap } from 'rxjs/operators';
+import { ApplicationState } from '../state/app.state';
 import { InitializeConfig } from './config.actions';
+import { ConfigSelectors } from './config.selectors';
 import { ConfigState } from './config.state';
 
 @Injectable({
@@ -14,5 +18,8 @@ export class ConfigEffects {
     .get<ConfigState>('/assets/config.json')
     .pipe(map(config => new InitializeConfig(config)));
 
-  constructor(private httpClient: HttpClient) {}
+  @Effect({ dispatch: false })
+  updateTitle = this.store.pipe(select(ConfigSelectors.getTitle)).pipe(tap(title => this.titleService.setTitle(title)));
+
+  constructor(private httpClient: HttpClient, private store: Store<ApplicationState>, private titleService: Title) {}
 }
